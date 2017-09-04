283dae08d83f0648e4630419ecf756f3ae976b0d
kanyalambi <kskhuba@gmail.com>
2017-09-04 20:46:53 +0530

    Function

---
+++ fun.py
@@ -0,0 +1,24 @@
+a = 50
+b = 10
+
+def sum():
+	sum = a + b
+	print "Sum = " ,sum
+def sub():
+	sub = a - b
+	print "Sub = " , sub
+def multi():
+	multi = a * b
+	print "Multiplication = " ,multi
+def Division():
+	divi = a / b
+	print "Division = " ,divi
+def mod_div():
+	modd = a % b
+	print "Mod division = " , modd
+
+sum()
+sub()
+multi()
+Division()
+mod_div()
