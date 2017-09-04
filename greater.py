3ddffa881d6475d41e72b889be02800d0510a30b
kanyalambi <kskhuba@gmail.com>
2017-09-04 20:49:36 +0530

    python basic programs

---
+++ greater.py
@@ -0,0 +1,10 @@
+# Greater of 3 numbers
+a = input("Enter value of a = ")
+b = input("Enter value of b = ")
+c = input("Enter value of c = ")
+if a > b:
+	print "a is  greater"
+elif b > c :
+	print "b is greaer"
+else:
+	print "c is greater"
